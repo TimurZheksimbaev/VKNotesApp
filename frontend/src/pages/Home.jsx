b90668f644414165a7f4fde835ad23d70368ce85
Timur Zheksimbaev <113367706+TimurZheksimbaev@users.noreply.github.com>
2024-05-12 12:38:41 +0300

    Added logout button

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,15 @@
 import { useState, useEffect } from "react";
 import "../styles/Home.css"
 import Notes from "../components/Notes"
+import { useNavigate } from "react-router-dom";
 
 
 function Home() {
     const username = localStorage.getItem('username')
+    const navigate = useNavigate()
     return <div>
         <h1>Welcome home {username}</h1>
+        <button onClick={() => navigate("/logout")} className="logout-button"> Logout </button>
         <Notes />
     </div>
 }
